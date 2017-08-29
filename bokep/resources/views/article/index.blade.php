598d4608e2b4b7a8ffda8c00c6d30836a65f42b1
ambrizals <sabuncolek@ambrizal.net>
2017-08-29 16:11:54 +0800

    Update 0.2.5

--- bokep/resources/views/article/index.blade.php
+++ bokep/resources/views/article/index.blade.php
@@ -6,17 +6,32 @@
 		@include('includes.sidebar_panel')
 	</div>
 	<div class="col-md-9 content-page">
-		<table class="table" id="daftarCategory">
-			<thead>
-				<tr>
-					<th>Judul Artikel</th>
-					<th>Kategori Artikel</th>
-					<th>Ditulis</th>
-					<th>Tanggal terbit</th>
-					<th>Tanggal Update</th>
-				</tr>
-			</thead>
-		</table>
+		<div class="card">
+			<div class="card-header">
+				<div class="row">
+					<div class="col-md-4">
+						Daftar Artikel
+					</div>
+					<div class="col-md-8 text-right">
+						<a href="#" class="btn btn-sm btn-primary">Buat Artikel</a>
+						<a href="#" class="btn btn-sm btn-primary">Backup Artikel</a>
+					</div>
+				</div>
+			</div>
+			<div class="card-body">
+				<table class="table table-responsive" id="daftarArticle">
+					<thead>
+						<tr>
+							<th>Judul Artikel</th>
+							<th>Kategori Artikel</th>
+							<th>Ditulis</th>
+							<th>Tanggal terbit</th>
+							<th>Tanggal Update</th>
+						</tr>
+					</thead>
+				</table>
+			</div>
+		</div>
 	</div>
 </div>
 @stop
@@ -24,7 +39,7 @@
 {{ Html::script('js/jquery.dataTables.min.js') }}
 <script>
 $(function() {
-    $('#daftarCategory').DataTable({
+    $('#daftarArticle').DataTable({
         processing: true,
         serverSide: true,
         ajax: '{!! route('Ajax Load Article') !!}',
