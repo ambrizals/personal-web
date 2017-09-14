6ec44a77d97add9c0fcb65bfcb7b09b7a92e6de7
ambrizals <sabuncolek@ambrizal.net>
2017-09-14 21:06:57 +0800

    Update

--- apps/resources/views/article/index_blog.blade.php
+++ apps/resources/views/article/index_blog.blade.php
@@ -5,10 +5,17 @@
 	<div class="col-md-8 blog-page">
 		@foreach ($article as $item)
 		<div class="article-box">
-			<h3><a href="{{ route('articles.show', $item->slug_article) }}">{!! $item->judul_article !!}</a></h3>
-			<p>Penulis : {!! $item->User->name !!} | Kategori : {!! $item->CategoryArticle->nama_kategori !!} | Tanggal terbit {!! $item->created_at !!}</p>
-			<div class="article-content">
-				{{ strip_tags(str_limit($item->konten_article,300)) }}
+			<div class="row">
+				<div class="col-4">
+					<img src="{!! url('/').'/img/thumbnail/'.$item->thumbnail_article !!}"/>
+				</div>
+				<div class="col-7">
+					<h3><a href="{{ route('articles.show', $item->slug_article) }}">{!! $item->judul_article !!}</a></h3>
+					<p>Penulis : {!! $item->User->name !!} | Kategori : {!! $item->CategoryArticle->nama_kategori !!} | Tanggal terbit {!! $item->created_at !!}</p>
+					<div class="article-content">
+						{{ strip_tags(str_limit($item->konten_article,300)) }}
+					</div>
+				</div>
 			</div>
 		</div>
 		@endforeach
